15de7e0f32455d9a6342f72ea07049b51e934d4c
mukesh <mukeshdixena123@gmail.com>
2025-11-01 20:30:26 +0530

    Fix api base url:

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,3 @@
 // Global API base URL
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-// export const API_BASE_URL = "/api";
+// export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL = "/api";
