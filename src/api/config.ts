321ede8520463671ce998afad7b24c913d3016c3
mukesh <mukeshdixena123@gmail.com>
2025-11-05 16:22:37 +0530

    Fix url for deplyement

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,4 @@
 // Global API base URL
 // export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const API_BASE_URL = "/api";
+export const API_BASE_URL = "3000/api";
 // export const API_BASE_URL = "https://tkqfrp6b-3000.asse.devtunnels.ms/";
